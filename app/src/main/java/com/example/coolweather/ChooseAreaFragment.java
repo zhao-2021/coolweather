43e32366963fe4a1b40469321b8b9418a360f197
zhao-2021 <15506619611@163.com>
2023-01-03 10:33:31 +0800

    first commit

---
+++ app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
@@ -0,0 +1,15 @@
+package com.example.coolweather;
+
+import android.app.ProgressDialog;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class ChooseAreaFragment extends Fragment {
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY = 2;
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    
+}
